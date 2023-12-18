60c379b830dc2322052d4667014765008daed52d
levywanke <levywanke@gmail.com>
2023-12-18 22:23:10 +0300

    monty files

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- empty
+  *@head: head
+  *@counter: numline
+  *Return: nthing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
